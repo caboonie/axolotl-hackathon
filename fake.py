c4e6a03f61d8bad8ad827464a20aaa4479aacf7f
Caleb Noble (Intern) <cnoble1@godaddy.com>
2020-07-21 16:10:07 -0600

    add timestamps

--- fake.py
+++ fake.py
@@ -1,14 +1,17 @@
 import requests
 import json
 import os
+import datetime
 
 # Make a get request to get the latest position of the international space station from the opennotify api.
 token = os.environ['GITHUB_TOKEN'] 
 headers={'Authorization': 'token {}'.format(token)}
 response = requests.get("https://api.github.com/repos/caboonie/axolotl-hackathon/pulls", headers=headers)
 
+PARSE_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
+
 content = json.loads(response.content)
-print(content)
+# print(content)
 pr_summary = []
 
 blocks = [{  
@@ -19,11 +22,9 @@ blocks = [{
         }}]
 for pr in content:
     changes = 0
-    # print('pr',content)
     status_response = requests.get(pr['url'] + '/files', headers=headers)
     files = json.loads(status_response.content)
     for file in files:
-        print('file',file)
         changes += file['changes']
     type_of_review = ''
     if changes < 1000:
@@ -36,12 +37,33 @@ for pr in content:
     jira_link = pr['body'].split('\n')[0].split(': ')[1]
     jira_ticket = jira_link.split('/')[-1]
 
+    requested_reviewers = [reviewer['login'] for reviewer in pr["requested_reviewers"]]
+    print('requested',requested_reviewers)
+    # print("review comments", pr['review_comments'])
+    review_response = requests.get(pr['url'] + '/reviews', headers=headers)
+    reviews = json.loads(review_response.content)
+    # print("reviews", reviews)
+
+    # print('pr',, datetime.datetime.utcnow()) #"updated_at", pushed_at
+    time_created = datetime.datetime.strptime(pr['created_at'], PARSE_FORMAT)
+    time_since_created = datetime.datetime.utcnow()-time_created
+    since_created_str = '{} days'.format(time_since_created.days)
+    if time_since_created.days < 1:
+        since_created_str = '{:.1f} hours'.format(time_since_created.seconds/3600)
+
+    time_updated = datetime.datetime.strptime(pr['updated_at'], PARSE_FORMAT)
+    time_since_updated = datetime.datetime.utcnow()-time_updated
+    since_updated_str = '{} days'.format(time_since_updated.days)
+    if time_since_updated.days < 1:
+        since_updated_str = '{:.1f} hours'.format(time_since_updated.seconds/3600)
+    
+
     blocks.append({  
         "type": "section",
         "block_id": pr['title'],
         "text": {  
             "type": "mrkdwn",
-            "text": "<{}|*{}*>\n{}  <{}|Jira {}>\nWaiting on: *{}* (suggested <@{}>)".format(pr['html_url'], pr['title'], type_of_review, jira_link, jira_ticket, 'Review', 'U017UV4FAG1')
+            "text": "<{}|*{}*>\n{}  <{}|Jira {}>\n{} old, {} since last action\nWaiting on: *{}* (suggested <@{}>)".format(pr['html_url'], pr['title'], type_of_review, jira_link, jira_ticket, since_created_str, since_updated_str, 'Review', 'U017UV4FAG1')
         }})
 
 slack_token = os.environ['SLACK_TOKEN'] 
