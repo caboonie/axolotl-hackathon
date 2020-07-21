003313e4dce6780ec90b089e7df2665ca2cc242b
Nithin Sagar Nallagula <nnallagula@godaddy.com>
2020-07-21 13:25:50 -0700

    added commits api call

--- fake.py
+++ fake.py
@@ -4,19 +4,27 @@ import os
 
 # Make a get request to get the latest position of the international space station from the opennotify api.
 response = requests.get("https://api.github.com/repos/caboonie/axolotl-hackathon/pulls")
-# Print the status code of the response.
-# print(response.content)
+
 content = json.loads(response.content)
-# print(len(content))
 pr_summary = []
 slack_info = ''
 slack_info = slack_info + 'Hi, here are the PRs for \n'
 for pr in content:
-	slack_info = slack_info + pr['body'] + ' - ' + pr['url'] + '\n'
-    #pr_summary.append(pr['title'])
+	changes = 0
+	status_response = requests.get(pr['url'] + '/files')
+	files = json.loads(status_response.content)
+	for file in files:
+		changes += file['changes']
+	type_of_review = ''
+	if changes < 1000:
+		type_of_review = 'Short Review(' + str(changes) + ')'
+	elif (changes < 2000 and changes > 1000):
+		type_of_review = 'Medium Review(' + str(changes) + ')'
+	else:
+		type_of_review = 'Long Review(' + str(changes) + ')'
+	slack_info = slack_info + pr['body'] + ' - ' + pr['url'] + ' - Type: ' + type_of_review +' - Reporter: <@U017UV4FAG1>' + '\n'
 
-print(pr_summary)
-slack_token = 'xoxb-1266930654353-1278198044544-VN1SnYmdtmKgHFXQvHVXBe3h'
+slack_token = 'xoxb-1266930654353-1278198044544-VTZeknhgwnxXk5uFaCkWbuhm'
 slack_channel = '#test'
 slack_icon_url = 'https://th.bing.com/th/id/OIP.ScZ7yk9J7_I9J166r5gLTwHaHa?pid=Api&rs=1'
 # slack_user_name = 'axolotl'
@@ -29,5 +37,6 @@ def post_message_to_slack(text, blocks = None):
         'icon_url': slack_icon_url,
         'blocks': json.dumps(blocks) if blocks else None
     }).json()	
+
 
 post_message_to_slack(slack_info)
