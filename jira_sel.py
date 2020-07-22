818a0916f429ccc6e183c815a8c4daad9053f536
Japneet Singh <jsingh1@godaddy.com>
2020-07-21 19:32:31 -0500

    integration

--- jira_sel.py
+++ jira_sel.py
@@ -3,6 +3,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 import time
+import configparser
+
+Config = configparser.ConfigParser()
+Config.read("./config.ini")
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")
@@ -10,16 +14,19 @@ chrome_options.add_argument("--window-size=1920x1080")
 
 driver = webdriver.Chrome(executable_path=r'C:/Users/jsingh1/Downloads/chromedriver_win32/chromedriver.exe', options=chrome_options)
 
-driver.get("https://github.secureserver.net/MCX/dotnet-api-packages/pull/207")
+def run(url):
 
-elem = driver.find_element_by_id("login_field")
-elem.send_keys("jsingh1")
-time.sleep(2)
-passw = driver.find_element_by_id("password")
-passw.send_keys("Sanjam@10")
-log = driver.find_element_by_name("commit")
-log.send_keys(Keys.ENTER)
-time.sleep(3)
-re = driver.find_elements_by_class_name("assignee")
-for ass in re:
-    print(ass.text)
+    driver.get(url)
+    reviewers = []
+    elem = driver.find_element_by_id("login_field")
+    elem.send_keys(Config['CREDENTIALS']["username"])
+    time.sleep(2)
+    passw = driver.find_element_by_id("password")
+    passw.send_keys(Config['CREDENTIALS']["password"])
+    log = driver.find_element_by_name("commit")
+    log.send_keys(Keys.ENTER)
+    time.sleep(3)
+    re = driver.find_elements_by_class_name("assignee")
+    for ass in re:
+        reviewers.append(ass)
+    return reviewers
